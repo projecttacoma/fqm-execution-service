35bd8544e906119274aa1415964e32c89f0a8ac4
Matthew Gramigna <mgramigna@mitre.org>
2020-07-24 16:20:46 -0400

    Update API and readme

--- src/types/server-types.d.ts
+++ src/types/server-types.d.ts
@@ -1,9 +1,10 @@
 import { R4 } from '@ahryman40k/ts-fhir-types';
 
+// Interface for the expected input to the calculate request
 export interface RequestBody {
   measure: R4.IBundle;
-  patient: R4.IBundle;
+  patient: R4.IBundle[];
   options?: {
-    [optionName: string]: any;
+    [optionName: string]: any; // TODO: Define the options we want to support
   };
 }
--- test/app.test.ts
+++ test/app.test.ts
@@ -4,8 +4,11 @@ import mockMeasure from './fixtures/mockMeasure.json';
 import mockPatient from './fixtures/mockPatient.json';
 
 // TODO: This is just an example test for demonstration
-test('patient and measure upload', async () => {
-  const response = await request(app).post('/upload').send({ measure: mockMeasure, patient: mockPatient }).expect(200);
+test('patient and measure calculate', async () => {
+  const response = await request(app)
+    .post('/calculate')
+    .send({ measure: mockMeasure, patient: mockPatient })
+    .expect(200);
 
   expect(response.body.measure).toEqual(mockMeasure);
   expect(response.body.patient).toEqual(mockPatient);
