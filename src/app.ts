e3fc8df8e2ccc13cdfb741d5e522d79f78b5652b
Michael O'Keefe <mokeefe@mitre.org>
2020-10-12 10:25:41 -0400

    Why are you breaking

--- src/app.ts
+++ src/app.ts
@@ -2,22 +2,24 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import { logger } from './utils/logger';
 import { RequestBody } from './types/server-types';
+import { Calculator } from 'fqm-execution';
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/calculate', (req, res) => {
+app.post('/calculateRaw', (req, res) => {
   const body = req.body as RequestBody;
 
   logger.info(`[${req.ip}] POST /calculate`);
 
   // TODO: Update return result to be a call to the library
-  const { measure, patient } = body;
-  return res.json({
+  const { measure, patients, options } = body;
+  const rawResult = Calculator.calculateRaw(
     measure,
-    patient
-  });
+    patients,
+    options || {}
+  );
 });
 
 export default app;
