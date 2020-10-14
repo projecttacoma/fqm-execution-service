d19e0cf6c19fd44b12a4faf4e7eca7e4217e4f47
Matthew Gramigna <mgramigna@mitre.org>
2020-10-14 12:37:01 -0400

    Update test and readme; rename server types

--- src/types/server-types.ts
+++ src/types/server-types.ts
@@ -1,9 +1,9 @@
 import { R4 } from '@ahryman40k/ts-fhir-types';
-import { CalculationOptions } from 'fqm-execution';
+import { CalculatorTypes } from 'fqm-execution';
 
 // Interface for the expected input to the calculate request
 export interface RequestBody {
   measure: R4.IBundle;
   patients: R4.IBundle[];
-  options?: CalculationOptions;
+  options?: CalculatorTypes.CalculationOptions;
 }
--- test/app.test.ts
+++ test/app.test.ts
@@ -1,15 +1,26 @@
+import { R4 } from '@ahryman40k/ts-fhir-types';
+import { Calculator, CQLTypes } from 'fqm-execution';
+import { when } from 'jest-when';
 import request from 'supertest';
 import app from '../src/app';
-import mockMeasure from './fixtures/mockMeasure.json';
-import mockPatient from './fixtures/mockPatient.json';
+
+const mockMeasureBundle = <R4.IBundle>{};
+const mockPatientBundles = <R4.IBundle[]>[];
+const mockRawResult: CQLTypes.Results = {
+  patientResults: {},
+  unfilteredResults: {},
+  localIdPatientResultsMap: {}
+};
+
+const calculateRawSpy = jest.spyOn(Calculator, 'calculateRaw');
+when(calculateRawSpy).calledWith(mockMeasureBundle, mockPatientBundles, {}).mockReturnValue(mockRawResult);
 
 // TODO: This is just an example test for demonstration
 test('patient and measure calculate', async () => {
   const response = await request(app)
-    .post('/calculate')
-    .send({ measure: mockMeasure, patient: mockPatient })
+    .post('/calculateRaw')
+    .send({ measure: mockMeasureBundle, patients: mockPatientBundles })
     .expect(200);
 
-  expect(response.body.measure).toEqual(mockMeasure);
-  expect(response.body.patient).toEqual(mockPatient);
+  expect(response.body).toEqual(mockRawResult);
 });
