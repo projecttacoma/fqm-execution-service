48e5389e40a8be98e0131be160068db1f45a99fa
Chris Hossenlopp <hossenlopp@mitre.org>
2021-08-17 17:34:03 -0400

    Switch to using @types/fhir for TS FHIR types

--- src/types/server-types.ts
+++ src/types/server-types.ts
@@ -1,15 +1,14 @@
-import { R4 } from '@ahryman40k/ts-fhir-types';
 import { CalculatorTypes } from 'fqm-execution';
 
 // Interface for the expected input to the calculate request
 export interface RequestBody {
-  measure: R4.IBundle;
-  patients: R4.IBundle[];
+  measure: fhir4.Bundle;
+  patients: fhir4.Bundle[];
   options?: CalculatorTypes.CalculationOptions;
 }
 
 // Interface for the expected input to the data-requirements request
 // Separate from the others, because it doesn't accept Patients or options
 export interface DataRequirementsBody {
-  measure: R4.IBundle;
+  measure: fhir4.Bundle;
 }
